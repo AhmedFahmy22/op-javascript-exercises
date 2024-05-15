37c205309dd0255bf9fcf25d7f8dda8927a81f6c
AhmedFahmy22 <ahmed.fahmy00@eng-st.cu.edu.eg>
2024-05-15 09:12:13 +0300

    JS exercise 05 solved

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = undefined;
 
+    if(num1>num2) {
+        num1 = num1 ^ num2;
+        num2 = num1 ^ num2;
+        num1 = num1 ^ num2;
+    }
+
+    if(!((typeof num1 === "number") && (typeof num2 === "number")) || (num1<0)) {
+        sum = "ERROR";
+    }
+    else {
+        sum = 0;
+        for(let cntr = num1; cntr<=num2; cntr++){
+            sum += cntr;
+        }
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
