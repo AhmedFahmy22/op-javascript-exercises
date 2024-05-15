f294637396a7e28b3c30d9de5092eb49a8bddd56
AhmedFahmy22 <ahmed.fahmy00@eng-st.cu.edu.eg>
2024-05-15 08:31:10 +0300

    JS exercise 03 solved

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let teststr = [];
+    let stringLen = string.length;
+    for(let cntr = 0; cntr<stringLen; cntr++) {
+        teststr[stringLen-cntr-1] = string[cntr];
+    }
+    return teststr.join('');
 };
 
 // Do not edit below this line
