6dd390f4102f7319e995efa6d9d7afb06f0f45d7
AhmedFahmy22 <ahmed.fahmy00@eng-st.cu.edu.eg>
2024-05-15 09:18:29 +0300

    JS exercise 06 solved

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let returnVal = false;
 
+    if(((year%4===0)&&(year%100!==0)) || (year%400===0)) {
+        returnVal = true;
+    }
+
+    return returnVal;
 };
 
 // Do not edit below this line
