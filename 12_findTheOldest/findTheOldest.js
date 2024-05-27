597aee11377dd8fcbd403e9d689f1bf777050654
AhmedFahmy22 <ahmed.fahmy00@eng-st.cu.edu.eg>
2024-05-28 02:46:27 +0300

    JS exercise 12 solved

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(objects) {
+    let maxAge = 0;
+    let maxIndx = -1;
+    let iteratorIndx=-1;
+    objects.map(function(object) {
+        if(!("yearOfDeath" in object)) {
+            object.yearOfDeath = new Date().getFullYear();
+        }
+        let age = object.yearOfDeath-object.yearOfBirth;
+        
+        iteratorIndx++;
+        if(maxAge<age) {
+            maxIndx = iteratorIndx;
+            maxAge = age;
+        }
+    });
+    return objects[maxIndx];
 };
 
 // Do not edit below this line
