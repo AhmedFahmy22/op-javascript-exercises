93dbc7d242816269541bfef7f89e9221b5ebc65f
AhmedFahmy22 <ahmed.fahmy00@eng-st.cu.edu.eg>
2024-05-28 01:53:13 +0300

    JS exercise 09 solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (stringInput) {
+    let stringArr = Array.from(stringInput.toUpperCase());
+    /*Filter*/
+    let filteredStringArr = stringArr.filter((input)=>(((input>='A')&&(input<='Z')) 
+                                                        || ((input>='a')&&(input<='z')) 
+                                                        || ((input>='0')&&(input<='9'))));
+    return (filteredStringArr.join("")===filteredStringArr.reverse().join(""));
 };
 
 // Do not edit below this line
