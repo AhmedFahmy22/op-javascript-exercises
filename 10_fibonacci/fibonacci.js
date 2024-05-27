fa6edad0f140798210800d988a7490c20e704f7e
AhmedFahmy22 <ahmed.fahmy00@eng-st.cu.edu.eg>
2024-05-28 02:13:20 +0300

    JS exercise 10 solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(fibIndx) {
+    let nums = [0,1];
+    let temp = 0;
+    if(fibIndx>=0){
+        while(fibIndx>0) {
+            fibIndx--;
+            temp=nums[0]+nums[1];
+            nums[0]=nums[1];
+            nums[1]=temp;
+        }
+    }
+    else{
+        nums[0]="OOPS"
+    }
+    return nums[0];
 };
 
 // Do not edit below this line
